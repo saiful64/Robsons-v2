295938e5bdcdb74d5ff0aa3d2e2b79677bbf2305
saiful64 <saifulislam778866@gmail.com>
2023-07-12 19:02:22 +0530

    Calender toggle in status

--- client/src/components/generate-status-view.jsx
+++ client/src/components/generate-status-view.jsx
@@ -20,7 +20,7 @@ function GenerateStatus() {
 	const [generateStatusData, setGenerateStatusData] = useState([]);
 	const [columns, setColumns] = useState([]);
 	const [data, setData] = useState([]);
-	const [showCalendar, setShowCalendar] = useState(false);
+	const [showCalendar, setShowCalendar] = useState(true);
 
 	useEffect(() => {
 		axios
@@ -36,15 +36,9 @@ function GenerateStatus() {
 	}, []);
 	const navigate = useNavigate();
 	const auth = useAuth();
-	const logout = () => {
-		auth.logout();
-		navigate("/login");
-	};
-	const goToFormPage = () => {
-		navigate("/forms");
-	};
-	const generateSheetPage = () => {
-		navigate("/generate-excel-sheet");
+
+	const handleClick = () => {
+		setShowCalendar(!showCalendar);
 	};
 
 	// Use useMemo to memoize the table instance to avoid unnecessary re-renders
@@ -58,9 +52,10 @@ function GenerateStatus() {
 			<div className='flex flex-col items-center justify-center h-screen'>
 				<div className='bg-white p-6 rounded-lg shadow-md w-full sm:w-96'>
 					<h1 className='text-3xl font-bold mb-4'>Welcome Consultant</h1>
-					<div className='flex flex-col justify-center mb-7'>
-						<div className='flex flex-col justify-center'>
-							{showCalendar && (
+
+					{showCalendar ? (
+						<div className='flex flex-col justify-center mb-7'>
+							<div className='flex flex-col justify-center'>
 								<DateRange
 									editableDateInputs={true}
 									onChange={(item) => setDateRange([item.selection])}
@@ -68,57 +63,59 @@ function GenerateStatus() {
 									maxDate={new Date()}
 									ranges={dateRange}
 								/>
-							)}
-							<button
-								className='bg-green-500 hover:bg-green-700 text-white font-bold p-2 rounded'
-								onClick={() => setShowCalendar(!showCalendar)}
-							>
-								{showCalendar ? "Close Calendar" : "Open Calendar"}
-							</button>
-						</div>
-					</div>
-					<div className='flex flex-col justify-center'>
-						<div className='overflow-x-auto'>
-							<table {...getTableProps()} className='table-auto w-full'>
-								<thead className='bg-gray-200 text-gray-600 uppercase text-xs leading-normal'>
-									{headerGroups.map((headerGroup) => (
-										<tr {...headerGroup.getHeaderGroupProps()}>
-											{headerGroup.headers.map((column) => (
-												<th
-													{...column.getHeaderProps()}
-													className='py-3 px-6 text-left'
-												>
-													{column.render("Header")}
-												</th>
-											))}
-										</tr>
-									))}
-								</thead>
-								<tbody
-									{...getTableBodyProps()}
-									className='text-gray-600 text-sm font-light'
+
+								<button
+									className='bg-green-500 hover:bg-green-700 text-white font-bold p-2 rounded'
+									onClick={() => handleClick()}
 								>
-									{rows.map((row) => {
-										prepareRow(row);
-										return (
-											<tr {...row.getRowProps()}>
-												{row.cells.map((cell) => {
-													return (
-														<td
-															{...cell.getCellProps()}
-															className='py-3 px-6 text-left whitespace-nowrap'
-														>
-															{cell.render("Cell")}
-														</td>
-													);
-												})}
-											</tr>
-										);
-									})}
-								</tbody>
-							</table>
+									OK
+								</button>
+							</div>
 						</div>
-					</div>
+					) : (
+						<div className='flex flex-col justify-center'>
+							<div className='overflow-x-auto'>
+								<table {...getTableProps()} className='table-auto w-full'>
+									<thead className='bg-gray-200 text-gray-600 uppercase text-xs leading-normal'>
+										{headerGroups.map((headerGroup) => (
+											<tr {...headerGroup.getHeaderGroupProps()}>
+												{headerGroup.headers.map((column) => (
+													<th
+														{...column.getHeaderProps()}
+														className='py-3 px-6 text-left'
+													>
+														{column.render("Header")}
+													</th>
+												))}
+											</tr>
+										))}
+									</thead>
+									<tbody
+										{...getTableBodyProps()}
+										className='text-gray-600 text-sm font-light'
+									>
+										{rows.map((row) => {
+											prepareRow(row);
+											return (
+												<tr {...row.getRowProps()}>
+													{row.cells.map((cell) => {
+														return (
+															<td
+																{...cell.getCellProps()}
+																className='py-3 px-6 text-left whitespace-nowrap'
+															>
+																{cell.render("Cell")}
+															</td>
+														);
+													})}
+												</tr>
+											);
+										})}
+									</tbody>
+								</table>
+							</div>
+						</div>
+					)}
 				</div>
 			</div>
 		</>
