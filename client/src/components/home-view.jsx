f6f1331946ff79d8dd7defaf26d1819f2960e9ad
saiful64 <saifulislam778866@gmail.com>
2023-07-30 22:13:47 +0530

    form1 update and logics update

--- client/src/components/home-view.jsx
+++ client/src/components/home-view.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "./auth";
 
 function HomeView() {
-
 	const navigate = useNavigate();
 	const auth = useAuth();
 	const logout = () => {
@@ -20,15 +19,15 @@ function HomeView() {
 		navigate("/generate-status");
 	};
 	const barChart = () => {
-		navigate("/bar-chart")
-	}
+		navigate("/bar-chart");
+	};
 	const lineChart = () => {
-		navigate("/line-chart")
-	}
+		navigate("/line-chart");
+	};
 	const dashboard = () => {
-		navigate("/dashboard")
-	}
-  const footerData = [
+		navigate("/dashboard");
+	};
+	const footerData = [
 		{ key: "mainText", displayText: "@ 2023 JIPMER, O & G  Dept." },
 		{ key: "subText", displayText: "Made with 🧡 by MCA students" },
 	];
@@ -40,14 +39,8 @@ function HomeView() {
 						{auth.user === "student" ? "Welcome Student" : "Welcome Doctor"}
 					</h1>
 					<div className='flex flex-col justify-center'>
-					<button
-							className='bg-zinc-300 hover:bg-gray-800 hover:text-white text-gray-500 font-medium hover:font-bold hover:shadow-xl py-2 px-4 rounded m-2 mb-6'
-							onClick={() => dashboard()}
-						>
-							DASHBOARD
-						</button>
 						<button
-							className='bg-zinc-300 hover:bg-gray-800 hover:text-white text-gray-500 font-medium hover:font-bold hover:shadow-xl py-2 px-4 rounded m-2 mb-6'
+							className='bg-zinc-300 hover:bg-gray-800 hover:text-white text-gray-600 font-medium hover:font-bold hover:shadow-xl py-2 px-4 rounded m-2 mb-6'
 							onClick={() => goToFormPage()}
 						>
 							CLASSIFY
@@ -55,29 +48,23 @@ function HomeView() {
 						{auth.user === "doctor" && (
 							<>
 								<button
-									className='bg-zinc-400 hover:bg-gray-600 hover:text-zinc-200 text-white font-medium hover:font-bold hover:shadow-xl py-2 px-4 rounded m-2 mb-6'
+									className='bg-zinc-400 hover:bg-gray-800 hover:text-white text-white font-medium hover:font-bold hover:shadow-xl py-2 px-4 rounded m-2 mb-6'
+									onClick={() => dashboard()}
+								>
+									DASHBOARD
+								</button>
+								<button
+									className='bg-zinc-500 hover:bg-gray-600 hover:text-zinc-200 text-white font-medium hover:font-bold hover:shadow-xl py-2 px-4 rounded m-2 mb-6'
 									onClick={() => recorderdata()}
 								>
 									RECORDED DATA
 								</button>
 								<button
-									className='bg-zinc-500 hover:bg-gray-600 text-white font-medium hover:font-bold hover:shadow-xl py-2 px-4 rounded m-2 mb-6'
+									className='bg-zinc-600 hover:bg-gray-600 text-white font-medium hover:font-bold hover:shadow-xl py-2 px-4 rounded m-2 mb-6'
 									onClick={() => analysis()}
 								>
 									ANALYSIS
 								</button>
-								{/* <button disabled
-									className='bg-zinc-500 hover:bg-gray-600 text-white font-medium hover:font-bold hover:shadow-xl py-2 px-4 rounded m-2 mb-6'
-									onClick={() => barChart()}
-								>
-									BAR CHART
-								</button>
-								<button disabled
-									className='bg-zinc-500 hover:bg-gray-600 text-white font-medium hover:font-bold hover:shadow-xl py-2 px-4 rounded m-2 mb-6'
-									onClick={() => lineChart()}
-								>
-									LINE CHART
-								</button> */}
 							</>
 						)}
 						<button
@@ -88,17 +75,16 @@ function HomeView() {
 						</button>
 					</div>
 				</div>
-        <div className="flex flex-col bottom-[4%] font-light absolute inset-x-0 mt-10 animate-pulse items-center justify-center">
-        {footerData.map((item) => (
-          <p key={item.key} className="text-md">
-            {item.displayText}
-          </p>
-        ))}
-      </div>
+				<div className='flex flex-col bottom-[4%] font-light absolute inset-x-0 mt-10 animate-pulse items-center justify-center'>
+					{footerData.map((item) => (
+						<p key={item.key} className='text-md'>
+							{item.displayText}
+						</p>
+					))}
+				</div>
 			</div>
 		</>
 	);
-
 }
 
 export default HomeView;
