dd953b6053f88d3dcfbf4695784bc4f4348b7413
SAIFUL ISLAM <48625015+saiful64@users.noreply.github.com>
2023-07-30 10:38:57 +0530

    Update config.js

--- client/src/components/config.js
+++ client/src/components/config.js
@@ -1,4 +1,4 @@
 // config.js
-const API_BASE_URL = "https://robsons-v2-omega.vercel.app/"; // Replace with your IP address and port
+const API_BASE_URL = "https://robsons-v2-omega.vercel.app"; // Replace with your IP address and port
 
 export default API_BASE_URL;
