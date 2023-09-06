7c15f0a3bd044beb6ffd2f165404afd53f351f60
saiful64 <saifulislam778866@gmail.com>
2023-09-06 11:51:49 +0530

    search feature added

--- client/src/components/PatientLog.jsx
+++ client/src/components/PatientLog.jsx
@@ -10,13 +10,13 @@ const PatientLog = () => {
 	const [currentPage, setCurrentPage] = useState(1);
 	const [patientsPerPage] = useState(10);
 	const [patientToDelete, setPatientToDelete] = useState(null);
+	const [searchQuery, setSearchQuery] = useState(""); // New state for search
 	const navigate = useNavigate();
 
 	const fetchPatients = () => {
 		fetch(`${API_BASE_URL}/api/patients`)
 			.then((response) => response.json())
 			.then((data) => {
-
 				const sortedPatients = data.sort((a, b) => {
 					const dateA = new Date(a.created_on);
 					const dateB = new Date(b.created_on);
@@ -36,7 +36,16 @@ const PatientLog = () => {
 
 	const indexOfLastPatient = currentPage * patientsPerPage;
 	const indexOfFirstPatient = indexOfLastPatient - patientsPerPage;
-	const currentPatients = patients.slice(
+
+	// Apply search filter to patients
+	const filteredPatients = patients.filter(
+		(patient) =>
+			(patient.patient_id && patient.patient_id.includes(searchQuery)) ||
+			(patient.patient_name &&
+				patient.patient_name.toLowerCase().includes(searchQuery.toLowerCase()))
+	);
+
+	const currentPatients = filteredPatients.slice(
 		indexOfFirstPatient,
 		indexOfLastPatient
 	);
@@ -110,12 +119,24 @@ const PatientLog = () => {
 		toast.info(`Viewing patient with ID : ${patient.patient_id}`);
 	};
 
+	const handleSearchChange = (event) => {
+		setSearchQuery(event.target.value);
+	};
+
 	return (
 		<>
 			<ToastContainer />
 			<div className='flex items-center justify-center h-screen'>
 				<div className='bg-white p-6 rounded-lg shadow-md w-96'>
 					<h1 className='text-3xl font-bold mb-4 text-center'>Patient Log</h1>
+					{/* Search input */}
+					<input
+						type='text'
+						placeholder='Search patients by ID or name'
+						value={searchQuery}
+						onChange={handleSearchChange}
+						className='w-full px-4 py-2 mb-4 border rounded-md'
+					/>
 					<table className='table-auto w-full'>
 						<thead>
 							<tr>
