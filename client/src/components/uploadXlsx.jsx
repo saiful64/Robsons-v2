d8bd9a8d5dd64c67b46de02a8f265562a979c3ee
VALiUM <srujansamal1@gmail.com>
2023-09-21 00:22:46 +0530

    excel upload styling.

--- client/src/components/uploadXlsx.jsx
+++ client/src/components/uploadXlsx.jsx
@@ -33,11 +33,31 @@ function UploadXlsx() {
   };
 
   return (
-    <div>
-      <h1>Upload XLSX File</h1>
-      <input type="file" accept=".xlsx" onChange={handleFileChange} />
-      <button onClick={handleUpload}>Upload</button>
+    <div className='flex flex-col items-center justify-center h-screen'>
+  <div className='bg-white  w-96 p-6 text-center h-2/5 rounded-lg drop-shadow-2xl flex flex-col justify-center'>
+    <h1 className='text-2xl font-bold mb-4 font-space'>Upload XLSX File</h1>
+    <div className='flex items-center justify-center mt-4'>
+      <input
+        className="block w-full text-sm text-slate-500 
+        file:mr-4 file:py-2 file:px-4
+        file:rounded-full file:border-0
+        file:text-sm file:font-semibold
+        file:bg-violet-50 file:text-violet-700
+        hover:file:bg-violet-100"
+        type="file"
+        accept=".xlsx"
+        onChange={handleFileChange}
+      />
+      <button
+        className='bg-violet-500 text-white py-2 px-4 rounded-full font-semibold hover:bg-violet-600'
+        onClick={handleUpload}
+      >
+        Upload
+      </button>
     </div>
+  </div>
+</div>
+
   );
 }
 
